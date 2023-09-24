b4cb4376d0b69db104c27305d4c7ee14b01018e9
Tony <112224948+ItsTonyy@users.noreply.github.com>
2023-09-23 23:43:59 -0300

    Numbers. BigInt left

---
+++ Módulo-02/Numbers/3 - Numbers.js
@@ -0,0 +1,9 @@
+"use strict";
+const num1 = 100.2;
+const num2 = 0x78CF;
+const num3 = 0o577;
+const num4 = 0b110001;
+console.log(`Normal Number: ${num1}`);
+console.log(`Hexadecimal: ${num2}`);
+console.log(`Octal: ${num3}`);
+console.log(`Binário: ${num4}`);
---
+++ Módulo-02/Numbers/3 - Numbers.ts
@@ -0,0 +1,12 @@
+const num1: number = 100.2
+const num2: number = 0x78CF
+const num3: number = 0o577
+const num4: number = 0b110001
+
+console.log(`Normal Number: ${num1}`)
+console.log(`Hexadecimal: ${num2}`)
+console.log(`Octal: ${num3}`)
+console.log(`Binário: ${num4}`)
+
+
+
