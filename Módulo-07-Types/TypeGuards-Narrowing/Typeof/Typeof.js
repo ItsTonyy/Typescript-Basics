885cced6d29c6c5487a88d24557413cd475558d1
Tony <112224948+ItsTonyy@users.noreply.github.com>
2023-11-29 18:49:29 -0300

    Typeof in Typescript

---
+++ Módulo-07-Types/TypeGuards-Narrowing/Typeof/Typeof.js
@@ -0,0 +1,7 @@
+var value = 'hello, i am a string';
+if (typeof value === 'string') {
+    console.log('trust me bro, this is a string');
+}
+else {
+    console.log('nah this is 100% a number brother, i can see it');
+}
---
+++ Módulo-07-Types/TypeGuards-Narrowing/Typeof/Typeof.ts
@@ -0,0 +1,9 @@
+let value: string | number = 'hello, i am a string'
+
+if (typeof value === 'string') {
+  console.log('trust me bro, this is a string')
+} else {
+  console.log('nah this is 100% a number brother, i can see it')
+}
+
+// output: 'trust me bro, this is a string'
