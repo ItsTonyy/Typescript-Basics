f9cfdd1729e4313cd007feb8b7197e3c331e32c9
Tony <112224948+ItsTonyy@users.noreply.github.com>
2023-12-14 00:09:49 -0300

    Record in Typescript

--- Módulo09_UtilityTypes/Pick/Pick.ts
+++ Módulo09_UtilityTypes/Pick/Pick.ts
@@ -6,8 +6,10 @@ interface computer {
   windowsVersion: number
 }
 
-type ComputerGpu = Pick<computer, 'gpu'>
+type ComputerGpu = Pick<computer, 'gpu' | 'cpu' | 'windowsVersion'>
 
 const gpu: ComputerGpu = {
-  gpu: 'Rtx 3080'
+  gpu: 'Rtx 3080',
+  cpu: 'intel i7',
+  windowsVersion: 11
 } 
---
+++ Módulo09_UtilityTypes/Record/Record.ts
@@ -0,0 +1,12 @@
+interface CatInfo {
+  age: number,
+  breed: string
+}
+
+type CatNames = 'Luke' | 'Luffy' | 'Banze'
+
+const cats: Record<CatInfo, CatNames> = {
+  'Luke': { age: 2, breed: 'Orange' },
+  'Luffy': { age: 10, breed: 'Persian' },
+  'Banze': { age: 12, breed: 'Viralata'}
+}
